47b9dbe4aed41e5ba11cac9d1b49d1dd5c631fd7
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2021-04-16 20:50:53 +0800

    fix  texture map

--- src/SceneObjects/Square.cpp
+++ src/SceneObjects/Square.cpp
@@ -43,14 +43,14 @@ bool Square::getObjUV(const ray& r, vec3f& u, vec3f& v) const
 	//std::cout << "aaa" << std::endl;
 	vec3f rP = r.getPosition();vec3f rD = r.getDirection();
 	vec3f p = transform->globalToLocalCoords(rP);
-	vec3f d = -p + transform->globalToLocalCoords(-rP + rD);
+	vec3f d = -p + transform->globalToLocalCoords(rP + rD);
 	d /= d.length();
 	isect i;
 	if (intersectLocal(ray(p,d), i))
 	{
 		vec3f P = p + i.t * d;
-		u = vec3f(P[0] + 0.5, 0, 1).clamp();
-		v = vec3f(P[1] + 0.5, 0, 1).clamp();
+		u = vec3f((P[0] + 0.5) , 0, 1).clamp();
+		v = vec3f((P[1] + 0.5) , 0, 1).clamp();
 		return true;
 	}
 	else return false;
